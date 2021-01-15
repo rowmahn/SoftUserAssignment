f5b1d07157518bc0d855429b9e972680c143b6c2
rowmahn <dulalroman1@gmail.com>
2021-01-15 12:04:53 +0545

    HomeFragment updated

--- app/src/main/java/com/example/softuserassignment/HomeFragment.kt
+++ app/src/main/java/com/example/softuserassignment/HomeFragment.kt
@@ -1,11 +1,29 @@
 package com.example.softuserassignment
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.softuserassignment.adapter.StudentAdapter
 
 class HomeFragment : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_fragment)
+
+    private lateinit var rView: RecyclerView
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val view = layoutInflater.inflate(R.layout.fragment_home, container, false)
+        rView = view?.findViewById(R.id.rView) as RecyclerView
+        val userAdapter = StudentAdapter(StudentListNavActivity.StudentList, this as Context)
+        rView!!.adapter = userAdapter
+        rView!!.layoutManager = LinearLayoutManager(this as Context?)
+        return view
     }
 }
